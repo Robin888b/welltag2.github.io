7a67e062363014e4c41f486d60a69682d16a319f
Robin <robin888b@outlook.com>
2022-07-05 05:48:18 +0200

    Improve design
    the site is now computer-compatible 🙃
    + you can now switch the color of the site (light / dark)
    + new colors

--- js/index.js
+++ js/index.js
@@ -19,3 +19,28 @@ function playSound(sound) {
     song1.pause();
   }
 }
+
+function toggleSpoiler(id) {
+  let spoiler = document.getElementById(id) // ← trouve le spoileur
+  let classes = spoiler.classList // ← classes du spoileur
+  if (classes == "spoiler"){ // ← si le spoileur n'est pas cliqué
+      spoiler.classList.add("clicked"); // ← ajout de la classe "clicked"
+      console.log(`the spoiler ${id} was clicked`)// ← log l'action
+  }
+}
+let theme = 1
+function toggleTheme(){
+  if (theme == 1){
+    document.documentElement.style.setProperty('--blue','#6AA6E2')
+    document.documentElement.style.setProperty('--backColor','#F2F2F2')
+    document.documentElement.style.setProperty('--backColorGradient','#D6D6D6')
+    document.documentElement.style.setProperty('--textColor','#000000')
+    theme = 0
+  }else{
+    document.documentElement.style.setProperty('--blue','#8BBAE9')
+    document.documentElement.style.setProperty('--backColor','#171120')
+    document.documentElement.style.setProperty('--backColorGradient','#ff83cd')
+    document.documentElement.style.setProperty('--textColor','#ffffff')
+    theme = 1
+  }
+}
